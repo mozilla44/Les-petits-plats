2d27e153d53cf75206da244d48f43d5135522ed9
antoine <antoine.frisco@gmail.com>
2022-04-08 09:33:57 +0200

    fonction recherche terminée

--- scripts/index.js
+++ scripts/index.js
@@ -3,89 +3,81 @@ import Recipe from "../model/Recipe.js";
 
 
 
-function searchRecipe() {
-    let input = document.getElementById('search-bar').value
-    input=input.toLowerCase();
-    let x = document.getElementsByClassName('recipe_card');
-    let result = []; 
-    for (let card of x){
-        card.remove()
-    }
-    for (let recipe of recipes){
-        if (recipe.name.includes(input)){
+function searchRecipe(input) {
+    let result = [];
+    for (let recipe of recipes) {
+        if (recipe.name.toLowerCase().includes(input)) {
             result.push(recipe)
         }
     }
-    
-    
-    displayRecipe(result)
+    return result
+
 }
 
 
 /* fonction d'affichage  */
-function displayRecipe (listRecipe){
+function displayRecipe(listRecipe) {
     let recipeDom = "";
-    for (let recipe of listRecipe ){
+    for (let recipe of listRecipe) {
         recipe = new Recipe(recipe)
         recipeDom += recipe.createRecipeCard();
     }
     document.getElementById("recipe-area").innerHTML = recipeDom;
 }
 
-let searchbar = document.getElementById('search-bar');
-searchbar.addEventListener("keyup" ,searchRecipe)
+
+
 
 
 /* search by ingredient */
 
-
-
-/* function SearchIngredients (){
-    let input = document.getElementById('search-bar').value
-    input=input.toLowerCase();
-    let x = document.getElementsByClassName('recipe_card');
-    let result = []; 
-    for (let card of x){
-        card.remove()
-    }
-    for (let recipe of recipes){
-        if (recipe.ingredient.includes(input)){
+function SearchIngredients(input) {
+    let result = [];
+    for (let recipe of recipes) {
+        if (recipe.ingredients.some(ingredient => ingredient.ingredient.toLowerCase().includes(input))) {
             result.push(recipe)
         }
     }
-    
-    
-    displayRecipe(result)
-} */
+    return result
+
+}
 
 /* search by description */
 
-/* function SearchDescription (){
-    let input = document.getElementById('search-bar').value
-    input=input.toLowerCase();
-    let x = document.getElementsByClassName('recipe_card');
-    let result = []; 
-    for (let card of x){
-        card.remove()
-    }
-    for (let recipe of recipes){
-        if (recipe.description.includes(input)){
+function SearchDescription(input) {
+    let result = [];
+    for (let recipe of recipes) {
+        if (recipe.description.toLowerCase().includes(input)) {
             result.push(recipe)
         }
     }
-    
-    
-    displayRecipe(result)
-} */
+    return result
 
+}
 /* eventlistener with all search functions */
 
-/* searchbar.addEventListener("keyup" ,() => {
-    searchRecipe();
-    SearchIngredients();
-    SearchDescription();
+let searchbar = document.getElementById('search-bar');
 
-}) */
+searchbar.addEventListener("keyup", () => {
+    let input = document.getElementById('search-bar').value
+    if (input.length > 2){
+    input = input.toLowerCase();
+    let y = document.getElementsByClassName('recipe_card');
+    let result = [];
+    for (let card of y) {
+        card.remove()
+    }
+    result = result.concat(searchRecipe(input));
+    result = result.concat(SearchIngredients(input));
+    result = result.concat(SearchDescription(input));
+
+    result = [...new Set(result)]
+    displayRecipe(result)
+    }
+    if (input.length == 0){
+        displayRecipe(recipes)
+    }
+})
 
 
 
